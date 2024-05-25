6aef8108c24b148b86afec8ef2175d7664baa947
Alfranciss W. Dionsay <118524388+TheAlfran@users.noreply.github.com>
2024-05-25 23:49:39 +0800

    feat: tanstack route (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import "./App.scss";
-import { Typography } from "@mui/material";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 function App() {
-  return <Typography>Hello World</Typography>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import {TanStackRouterVite} from '@tanstack/router-vite-plugin'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
 })
  
