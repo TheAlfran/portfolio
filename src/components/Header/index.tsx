29a25ebfcd6143c18a497e915789d5ad5664ae38
Alfranciss W. Dionsay <118524388+TheAlfran@users.noreply.github.com>
2024-05-29 02:11:56 +0800

    feat: initial header (#7)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,49 @@
-import { Box, Grid, Stack, Typography } from "@mui/material";
+import { Box, Button, Grid, Slide, Stack, Typography } from "@mui/material";
+import CallMadeIcon from "@mui/icons-material/CallMade";
 
 export default function Header() {
   return (
     <Grid container>
-      <Grid item md={12}>   
-        <Stack bgcolor={"blue"} height={70} mb={2} direction={"row"}>
-          <Box>
-          </Box>
-          <Box display={"flex"} >
-            <Typography>About</Typography>
-            <Typography>Portfolio</Typography>
-            <Typography>Contact</Typography>
-          </Box>
-        </Stack>
+      <Grid item md={12}>
+        <Slide
+          direction="down"
+          in={true}
+          mountOnEnter
+          unmountOnExit
+          timeout={1000}
+        >
+          <Stack
+            bgcolor="black"
+            height="92px"
+            mb={2}
+            direction="row"
+            alignItems="center"
+            justifyContent="space-evenly"
+          >
+            <img src="/public/inverted-logo.png" width="62px" height="53px" />
+            <Box display="flex" gap="40px" alignItems="center">
+              <Typography color="white">About</Typography>
+              <Typography color="white">Portfolio</Typography>
+              <Typography color="white">Contact</Typography>
+              <Button
+                sx={{
+                  border: 1,
+                  borderColor: "white",
+                  borderRadius: "20px",
+                  gap: "10px",
+                  textTransform: "none",
+                  pl: "16px",
+                  pr: "16px",
+                }}
+              >
+                <Typography color="white" fontSize="16px" fontWeight="none">
+                  View Resume
+                </Typography>
+                <CallMadeIcon sx={{ color: "white", fontSize: "14px" }} />
+              </Button>
+            </Box>
+          </Stack>
+        </Slide>
       </Grid>
     </Grid>
   );
