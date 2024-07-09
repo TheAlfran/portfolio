7a0c1c70aca766052ed8c1e008bf08b186df263c
TheAlfran <alfranexx25@gmail.com>
2024-07-10 00:00:29 +0800

    chore: remove unecessary space in imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { ThemeProvider } from "@mui/material";
 
 import { useCreateTheme } from "./@core";
-
 import "./App.scss";
 import { routeTree } from "./routeTree.gen";
 import { Footer, Header} from "./components";
