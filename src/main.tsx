9731d3ad2d944d0c5559402b1bc34dc964f54c71
Alfranciss W. Dionsay <118524388+TheAlfran@users.noreply.github.com>
2024-10-09 01:06:50 +0800

    feat: add analytics (#35)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from '@vercel/analytics/react';
 import { CssBaseline } from "@mui/material";
 
 import App from "./App.tsx";
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CssBaseline />
     <App />
+    <Analytics />
   </React.StrictMode>
 );
