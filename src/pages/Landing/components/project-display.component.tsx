307523642049b52122f7427cc943c881adcdd114
TheAlfran <alfranexx25@gmail.com>
2024-10-08 13:37:23 +0800

    feat: tooltip in progress component

--- src/pages/Landing/components/project-display.component.tsx
+++ src/pages/Landing/components/project-display.component.tsx
@@ -1,4 +1,4 @@
-import { Paper, Typography, Box, useTheme, Grow } from "@mui/material";
+import { Paper, Typography, Box, useTheme, Grow, Tooltip } from "@mui/material";
 import React from "react";
 
 interface ProjectDisplayComponentProps {
@@ -18,71 +18,74 @@ export const ProjectDisplayComponent: React.FC<
 }) => {
   const theme = useTheme();
   const [hover, setHover] = React.useState(false);
+
   return (
-    <Paper
-      elevation={hover ? 6 : 3}
-      sx={{
-        width: "265px",
-        height: "200px",
-        alignItems: "center",
-        display: "flex",
-        flexDirection: "column",
-        padding: "12px",
-        bgcolor: theme.palette.background.default,
-        transition: "elevation 0.3s ease",
-        "&:hover": {
-          ".grow-image": {
-            transform: "scaleX(1)",
+    <Tooltip title="In Progress" disableInteractive placement="top">
+      <Paper
+        elevation={hover ? 6 : 3}
+        sx={{
+          width: "265px",
+          height: "200px",
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+          padding: "12px",
+          bgcolor: theme.palette.background.default,
+          transition: "elevation 0.3s ease",
+          "&:hover": {
+            ".grow-image": {
+              transform: "scaleX(1)",
+            },
           },
-        },
-      }}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
-      <Box
-        width="100%"
-        height="126px"
-        component="img"
-        src={img}
-        bgcolor={theme.palette.secondary.main}
-        sx={{ objectFit: objectFit }}
-      />
-      <Box
-        width="100%"
-        mt="10px"
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
+        }}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
       >
-        <Box>
-          <Typography
-            fontSize="18px"
-            fontWeight="bold"
-            textAlign="start"
-            color={theme.palette.text.primary}
-          >
-            {title}
-          </Typography>
-          <Typography
-            fontSize="10px"
-            textAlign="start"
-            color={theme.palette.text.secondary}
-          >
-            {subtitle}
-          </Typography>
+        <Box
+          width="100%"
+          height="126px"
+          component="img"
+          src={img}
+          bgcolor={theme.palette.secondary.main}
+          sx={{ objectFit: objectFit }}
+        />
+        <Box
+          width="100%"
+          mt="10px"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Box>
+            <Typography
+              fontSize="18px"
+              fontWeight="bold"
+              textAlign="start"
+              color={theme.palette.text.primary}
+            >
+              {title}
+            </Typography>
+            <Typography
+              fontSize="10px"
+              textAlign="start"
+              color={theme.palette.text.secondary}
+            >
+              {subtitle}
+            </Typography>
+          </Box>
+          <Box display="flex" alignItems="center">
+            <Grow in={hover} timeout={500}>
+              <Box
+                component="img"
+                src="/next.png"
+                width="30px"
+                height="30px"
+                className="grow-image"
+              />
+            </Grow>
+          </Box>
         </Box>
-        <Box display="flex" alignItems="center">
-          <Grow in={hover} timeout={500}>
-            <Box
-              component="img"
-              src="/next.png"
-              width="30px"
-              height="30px"
-              className="grow-image"
-            />
-          </Grow>
-        </Box>
-      </Box>
-    </Paper>
+      </Paper>
+    </Tooltip>
   );
 };
--- src/pages/Portfolio/components/portfolio-card.tsx
+++ src/pages/Portfolio/components/portfolio-card.tsx
@@ -1,57 +1,71 @@
 import { useState } from "react";
-import { Box, Paper, Stack, Typography, useTheme, Grow } from "@mui/material";
+import {
+  Box,
+  Paper,
+  Stack,
+  Typography,
+  useTheme,
+  Grow,
+  Tooltip,
+} from "@mui/material";
 
 export const PortfolioCardComponent = () => {
   const [hover, setHover] = useState(false);
   const theme = useTheme();
 
   return (
-    <Paper
-      elevation={hover ? 6 : 3}
-      sx={{
-        width: "265px",
-        height: "210px",
-        padding: "9px",
-        backgroundColor: theme.palette.background.default,
-        mt: "20px",
-        position: "relative",
-        overflow: "hidden",
-        transition: "elevation 0.3s ease",
-        "&:hover": {
-          ".grow-image": {
-            transform: "scaleX(1)",
+    <Tooltip title="In Progress" arrow placement="top" disableInteractive>
+      <Paper
+        elevation={hover ? 6 : 3}
+        sx={{
+          width: "265px",
+          height: "210px",
+          padding: "9px",
+          backgroundColor: theme.palette.background.default,
+          mt: "20px",
+          position: "relative",
+          overflow: "hidden",
+          transition: "elevation 0.3s ease",
+          "&:hover": {
+            ".grow-image": {
+              transform: "scaleX(1)",
+            },
           },
-        },
-      }}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
-      <Box width="247px" height="130px" bgcolor={theme.palette.default.main} />
-      <Stack
-        display="flex"
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        mt="9px"
+        }}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
       >
-        <Stack>
-          <Typography fontSize="18px">Lorem ipsum</Typography>
-          <Typography fontSize="12px" color={theme.palette.text.secondary}>
-            Dolor
-          </Typography>
+        <Box
+          width="247px"
+          height="130px"
+          bgcolor={theme.palette.default.main}
+        />
+        <Stack
+          display="flex"
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          mt="9px"
+        >
+          <Stack>
+            <Typography fontSize="18px">Lorem ipsum</Typography>
+            <Typography fontSize="12px" color={theme.palette.text.secondary}>
+              Dolor
+            </Typography>
+          </Stack>
+          <Stack>
+            <Grow in={hover} timeout={500}>
+              <Box
+                component="img"
+                src="/next.png"
+                width="30px"
+                height="30px"
+                className="grow-image"
+              />
+            </Grow>
+          </Stack>
         </Stack>
-        <Stack>
-          <Grow in={hover} timeout={500}>
-            <Box
-              component="img"
-              src="/next.png"
-              width="30px"
-              height="30px"
-              className="grow-image"
-            />
-          </Grow>
-        </Stack>
-      </Stack>
-    </Paper>
+      </Paper>
+    </Tooltip>
   );
 };
